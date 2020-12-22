6331645d62c7888c90d1211839605639ddaaf846
Clyde D'Souza <clyd94@gmail.com>
2020-12-22 23:50:42 +1300

    Add comments to public facing objects

--- Src/Horoscope/Constants/ZodiacSignsEnum.cs
+++ Src/Horoscope/Constants/ZodiacSignsEnum.cs
@@ -5,17 +5,53 @@
     /// </summary>
     public enum ZodiacSigns
     {
+        /// <summary>
+        /// Pisces
+        /// </summary>
         Pisces,
+        /// <summary>
+        /// Aquarius
+        /// </summary>
         Aquarius,
+        /// <summary>
+        /// Virgo
+        /// </summary>
         Virgo,
+        /// <summary>
+        /// Taurus
+        /// </summary>
         Taurus,
+        /// <summary>
+        /// Scorpio
+        /// </summary>
         Scorpio,
+        /// <summary>
+        /// Sagittarius
+        /// </summary>
         Sagittarius,
+        /// <summary>
+        /// Libra
+        /// </summary>
         Libra,
+        /// <summary>
+        /// Leo
+        /// </summary>
         Leo,
+        /// <summary>
+        /// Gemini
+        /// </summary>
         Gemini,
+        /// <summary>
+        /// Capricorn
+        /// </summary>
         Capricorn,
+        /// <summary>
+        /// Cancer
+        /// </summary>
         Cancer,
+        /// <summary>
+        /// Aries
+        /// </summary>
         Aries
     }
 }
