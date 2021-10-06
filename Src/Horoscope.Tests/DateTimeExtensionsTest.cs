b3df0cdf87dc49059d65fbe41ffaa781a56e422b
Lucas Mundim <lucas.mundim044@gmail.com>
2021-10-06 10:34:41 -0300

    add extension method options and tests

---
+++ Src/Horoscope.Tests/DateTimeExtensionsTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+
+namespace Horoscope.Tests
+{
+    public class DateTimeExtensionsTest
+    {
+        [Theory]
+        [InlineData(2, 12, "Aquarius")]
+        [InlineData(3, 3, "Pisces")]
+        [InlineData(2, 28, "Pisces")]
+        [InlineData(10, 23, "Scorpio")]
+        public void GetZodiacSign_Test(int month, int day, string zodiacSign)
+        {
+            var date = new DateTime(1950, month, day);
+            var symbol = date.GetZodiacSign();
+
+            Assert.Equal(zodiacSign, symbol.ZodiacName);
+        }
+
+        [Theory]
+        [InlineData(2018, "Dog")]
+        [InlineData(1952, "Dragon")]
+        [InlineData(1969, "Rooster")]
+        [InlineData(1903, "Rabbit")]
+        public void GetChineseZodiacSign_Test(int year, string zodiacSign)
+        {
+            var date = new DateTime(year, 4, 1);
+            var symbol = date.GetChineseZodiacSign();
+
+            Assert.Equal(zodiacSign, symbol.ZodiacEnglishTranslation);
+        }
+    }
+}
