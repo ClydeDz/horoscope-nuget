edaf8264eeee48456124cac3d14403918c34f385
Lucas Fonseca Mundim <lucas.mundim044@gmail.com>
2021-10-30 01:22:31 -0300

    Add extension methods option (#13)

---
+++ Src/Horoscope.Tests/DateTimeExtensionsTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Horoscope.Extensions;
+using Xunit;
+
+namespace Horoscope.Tests
+{
+    public class DateTimeExtensionsTest
+    {
+        [Theory]
+        [InlineData(2, 12, "Aquarius")]
+        [InlineData(3, 3, "Pisces")]
+        [InlineData(2, 28, "Pisces")]
+        [InlineData(10, 23, "Scorpio")]
+        public void GetZodiacSign_Test(int month, int day, string zodiacSign)
+        {
+            var dateTime = new DateTime(1950, month, day);
+            var symbol = dateTime.GetZodiacSign();
+            Assert.Equal(zodiacSign, symbol.ZodiacName);
+        }
+
+        [Theory]
+        [InlineData(2018, "Dog")]
+        [InlineData(1952, "Dragon")]
+        [InlineData(1969, "Rooster")]
+        [InlineData(1903, "Rabbit")]
+        public void GetChineseZodiacSign_Test(int year, string zodiacSign)
+        {
+            var dateTime = new DateTime(year, 4, 1);
+            var symbol = dateTime.GetChineseZodiacSign();
+            Assert.Equal(zodiacSign, symbol.ZodiacEnglishTranslation);
+        }
+    }
+}
---
+++ Src/Horoscope/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Horoscope.Model;
+using System;
+
+namespace Horoscope.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Gets the zodiac sign for the date supplied.
+        /// </summary>
+        /// <param name="dateTime">The date for which you want the zodiac sign.</param>
+        /// <returns>A zodiac sign object.</returns>
+        public static ZodiacModel GetZodiacSign(this DateTime dateTime) => Zodiac.GetZodiacSignForDate(dateTime);
+
+        /// <summary>
+        /// Get the Chinese zodiac sign for the supplied date.
+        /// </summary>
+        /// <param name="dateTime">The date you want to query.</param>
+        /// <returns>Returns a Chinese zodiac sign object.</returns>
+        public static ChineseZodiacModel GetChineseZodiacSign(this DateTime dateTime) => ChineseZodiac.GetZodiacSignForDate(dateTime);
+    }
+}
